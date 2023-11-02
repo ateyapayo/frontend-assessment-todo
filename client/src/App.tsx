e29e18d6ed131c2a25c85460403a0519647c0c5a
Andrea Piano <andreapiano@Andreas-MacBook-Pro.local>
2023-11-02 13:15:03 +0000

    feat: initialised app

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+import "@mantine/core/styles.css";
+
+import { MantineProvider } from "@mantine/core";
+
+const App = () => {
+  return <MantineProvider>THIS IS MANTINE</MantineProvider>;
+};
+
+export default App;
